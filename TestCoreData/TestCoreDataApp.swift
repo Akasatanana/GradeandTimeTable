9017b503291a5cb4d6eba414497daeb1f3eb716a
akasatanana <akasatanana0894@gmail.com>
2022-05-07 17:31:46 +0900

    Initial Commit

---
+++ TestCoreData/TestCoreDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  TestCoreDataApp.swift
+//  TestCoreData
+//
+//  Created by Akasatanana on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct TestCoreDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
